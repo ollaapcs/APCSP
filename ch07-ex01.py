1e9f950b995da0848cb052ac0d201fb02f2cf576
ollaapcs <shernandez@bobcats.olla.org>
2016-09-20 10:24:09 -0400

    ch07-ex01

---
+++ ch07-ex01.py
@@ -0,0 +1,7 @@
+#Complete lines 1 and 2 below to create the code to add up all the numbers from 1 to 5 and print the sum.
+
+sum =
+thingsToAdd =
+for number in thingsToAdd:
+    sum = sum + number
+print(sum)
